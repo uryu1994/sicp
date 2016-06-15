9a59e69784754c45458626f51b44942f883b8ae0
uryu1994 <prices_over@uryu1994.work>
2016-06-15 21:08:11 +0900

    Add 1_20.scm

---
+++ 1_20.scm
@@ -0,0 +1,57 @@
+(gcd 206 40)
+(gcd 40 (remainder 206 40))
+(gcd 40 6)
+(gcd 6 (remainder 40 6))
+(gcd 6 4)
+(gcd 4 (remainder 6 4))
+(gcd 4 2)
+(gcd 2 (remainder 4 2))
+(gcd 2 0)
+;; 2 4回
+
+(gcd 206 40)
+
+(if (= 40 0)
+    206
+    (gcd 40 (remainder 206 40)))
+;; 0
+
+(if (= (remainder 206 40) 0)
+    40
+    (gcd (remainder 206 40) (remainder 40 (remainder 206 40))))
+;; 1
+
+(if (= (remainder 40 (remainder 206 40)) 0)
+    (remainder 206 40)
+    (gcd (remainder 40 (remainder 206 40))
+         (remainder (remainder 206 40)
+                    (remainder 40 (206 40)))))
+;; 2
+
+(if (= (remainder (remainder 206 40)
+                  (remainder 40 (remainder 206 40))) 0)
+    (gcd (remainder (remainder 206 40)
+                    (remainder 40 (remainder 206 40)))
+         (remainder (remainder 40 (remainder 206 40))
+                    (remainder (remainder 206 40)
+                               (remainder 40 (206 40))))))
+;; 4
+
+(if (= (remainder (remainder 40 (remainder 206 40))
+                    (remainder (remainder 206 40)
+                               (remainder 40 (remainder 206 40)))) 0)
+
+    (remainder (remainder 206 40)
+               (remainder 40 (remainder 206 40)))
+
+    (gcd (remainder (remainder 40 (remainder 206 40))
+                    (remainder (remainder 206 40)
+                               (remainder 40 (206 40))))
+
+         (remainder (remainder (remainder 206 40)
+                               (remainder 40 (remainder 206 40)))
+                    (remainder (remainder 40 (remainder 206 40))
+                               (remainder (remainder 206 40)
+                                          (remainder 40 (remainder 206 40)))))))
+;; 7+4
+;; 18回

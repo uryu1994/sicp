432fea361086683bebc86f7e473f5bd7991eff6d
uryu1994 <prices_over@uryu1994.work>
2016-10-05 23:20:17 +0900

    Add 2.29c

--- 2_29.scm
+++ 2_29.scm
@@ -17,21 +17,48 @@
 (define (branch-structure b)
   (car (cdr b)))
 
+(define mobile-balanced
+  (make-mobile (make-branch 2 3)
+               (make-branch 1
+                            (make-mobile (make-branch 1 3)
+                                         (make-branch 1 3)))))
+
 ;; 2.29b
 (define (branch-weight b)
   (let ((structure (branch-structure b)))
-    (if (number? structure)
+    (if (weight? structure)
 	structure
 	(total-weight structure))))
 
+(define weight? number?)
+
 (define (total-weight m)
   (let ((left (left-branch m))
-	(right (right-branch m)))
+        (right (right-branch m)))
     (+ (branch-weight left)
        (branch-weight right))))
 
-(define 
+;; 2.29c
+(define (balanced? m)
+  (let ((l (left-branch m))
+        (r (right-branch m)))
+    (and (= (moment l) (moment r))
+         (bweight? l)
+         (bweight? r))))
+
+(define (bweight? b)
+  (if (weight? (branch-structure b))
+      #t
+      (balanced? (branch-structure b))))
+        
+(define (moment b)
+  (* (branch-weight b) (branch-length b)))
+     
+
+(balanced? mobile-balanced)
 
 (define m2 (make-mobile (make-branch 6 2)
 			(make-branch 1 (make-mobile (make-branch 2 4)
-					(make-branch 1 8)))))
+                                        (make-branch 1 8)))))
+
+(balanced? m2)

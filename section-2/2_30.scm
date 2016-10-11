03749642dbabfd53fb9819b05861ec290d713972
uryu1994 <prices_over@uryu1994.work>
2016-10-12 01:37:41 +0900

    Fix 2_30 - 2_34

--- section-2/2_30.scm
+++ section-2/2_30.scm
@@ -1,5 +1,6 @@
 (define nil '())
 
+;; map版
 (define (square-tree tree)
   (map (lambda (sub-tree)
 	 (if (pair? sub-tree)
@@ -7,13 +8,14 @@
 	     (square sub-tree)))
        tree))
 
-(define (square-tree-2 tree)
+;; 直接版
+(define (square-tree tree)
   (cond ((null? tree) nil)
 	((not (pair? tree)) (square tree))
 	(else (cons (square-tree-2 (car tree))
 		    (square-tree-2 (cdr tree))))))
 
-(square-tree-2
+(square-tree
  (list 1
        (list 2 (list 3 4) 5)
        (list 6 7)))
--- section-2/2_31.scm
+++ section-2/2_31.scm
@@ -3,13 +3,13 @@
 (define (tree-map factor tree)
   (map (lambda (sub-tree)
 	 (if (pair? sub-tree)
-	     (tree-map factor  sub-tree)
+	     (tree-map factor sub-tree)
 	     (factor sub-tree)))
        tree))
 
-(define tree (square-tree
- (list 1
-       (list 2 (list 3 4) 5)
-       (list 6 7))))
+(define tree
+  (list 1
+        (list 2 (list 3 4) 5)
+        (list 6 7)))
 
-(tree-map (lambda (x) (* x x)) tree)
+(square-tree tree)

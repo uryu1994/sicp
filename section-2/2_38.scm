62ff13733dc47b9d61ee541d9256c70cb1bdf54e
uryu1994 <prices_over@uryu1994.work>
2016-10-17 02:13:39 +0900

    Add 2_38

---
+++ section-2/2_38.scm
@@ -0,0 +1,21 @@
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(define nil '())
+
+(fold-right / 1 (list 1 2 3))
+; => 3/2
+
+(fold-left / 1 (list 1 2 3))
+; => 1/6
+
+(fold-right list nil (list 1 2 3))
+; => (1 (2 (3 ())))
+
+(fold-left list nil (list 1 2 3))
+; => (((() 1) 2) 3)

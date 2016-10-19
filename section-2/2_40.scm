ffd13e1d40f3459d01c1baa1322ca94668cabdde
uryu1994 <prices_over@uryu1994.work>
2016-10-19 20:23:17 +0900

    Fix 2_40

--- section-2/2_40.scm
+++ section-2/2_40.scm
@@ -29,16 +29,16 @@
   (accumulate append nil (map proc seq)))
 
 (define (unique-pairs n)
-  (accumulate append
-              nil
-              (map (lambda (i)
-                     (map (lambda (j) (list i j))
-                          (enumerate-interval 1 (- i 1))))
-                   (enumerate-interval 1 n))))
+  (flatmap (lambda (i)
+             (map (lambda (j) (list i j))
+                  (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
 
 (define (prime-sum-pairs n)
   (map make-pair-sum
        (filter prime-sum?
                (unique-pairs n))))
 
-(prime-sum-pairs 6)
+(unique-pairs 6)
+
+(prime-sum-pairs 10)

4a346457c4013719400b5191b93e98ae8ae98ded
uryu1994 <prices_over@uryu1994.work>
2016-11-07 01:24:25 +0900

    Add 2_57, 2_58a

---
+++ section-2/2_57.scm
@@ -0,0 +1,13 @@
+(load "./2_56")
+
+(define (augend s)
+  (if (null? (cdddr s))
+      (caddr s)
+      (cons '+ (cddr s))))
+
+(define (multiplicand p)
+  (if (null? (cdddr p))
+      (caddr p)
+      (cons '* (cddr p))))
+
+(deriv '(* x y (+ x 3)) 'x)

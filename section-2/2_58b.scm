565a6d3705d987e029b7651f93f551b5a66dba5d
uryu1994 <prices_over@uryu1994.work>
2016-11-08 00:20:19 +0900

    Add 2_58b

---
+++ section-2/2_58b.scm
@@ -0,0 +1,37 @@
+(load "./2_58a")
+
+;;(x + 3 * (x + y + 2))
+;;addend -> x, augend -> 3 * (x + y + z)
+
+;;3 * (x + y + z)
+;;multiplier -> 3 multiplicand -> (x + y + z)
+
+;;x + y + z
+;;addend -> x augend -> y + z
+
+;;y + z
+;;addend -> y augend -> z
+
+(define (sum? x)
+  (if (eq? #f (memq '+ x))
+      #f
+      #t))
+
+(define (addend x)
+  (define (iter head tail)
+    (cond ((null? tail) (reverse head))
+          ((eq? '+ (car tail)) (reverse head))
+          (else (iter (cons (car tail) head)
+                      (cdr tail)))))
+  (let ((ans (iter '() x)))
+    (if (eq? 1 (length ans))
+        (car ans)
+        ans)))
+
+(define (augend x)
+  (let ((ans (cdr (memq '+ x))))
+    (if (eq? 1 (length ans))
+        (car ans)
+        ans)))
+
+(deriv '(x + 3 * (x + y + 2)) 'x)

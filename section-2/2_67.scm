0eb28217b205ccdf7cab967ac001f886b28f0d16
uryu1994 <prices_over@uryu1994.work>
2016-12-04 13:18:31 +0900

    delete print code

--- section-2/2_67.scm
+++ section-2/2_67.scm
@@ -8,7 +8,6 @@
                                    (make-leaf 'C 1)))))
 
 (define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
-(print sample-tree)
 ;;((leaf A 4) ((leaf B 2) ((leaf D 1) (leaf C 1) (D C) 2) (B D C) 4) (A B D C) 8)
 
 (decode sample-message sample-tree)

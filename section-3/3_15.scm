2526d5c27b0614651240d262a436b97ff246ec6f
uryu1994 <prices_over@uryu1994.work>
2017-03-28 00:41:14 +0900

    Add 3.15

---
+++ section-3/3_15.scm
@@ -0,0 +1,12 @@
+(define x (list 'a 'b))
+(define z1 (cons x x))
+(define z2 (cons (list 'a 'b) (list 'a 'b)))
+
+(define (set-to-wow! x)
+  (set-car! (car x) 'wow)
+  x)
+
+(print z1)
+(set-to-wow! z1)
+(print z2)
+(set-to-wow! z2)

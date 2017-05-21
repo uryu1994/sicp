96a0125fdfc4ba06b353553c91feb08e5e210e4a
uryu1994 <prices_over@uryu1994.work>
2017-05-22 01:48:31 +0900

    Calc Delay of 3.30

--- section-3/3_30.scm
+++ section-3/3_30.scm
@@ -4,3 +4,20 @@
         (full-adder (car ak) (car bk) c (car sk) c-out)
         (ripple-carry-adder (cdr ak) (cdr bk) (cdr-sk) c-out))
       'ok))
+
+;; Da = and-gate-delay
+;; Do = or-gate-delay
+;; Di = inverter-delay
+;; Dh = half-adder-delay
+;; Df = full-adder-delay
+;; Dr = ripple-carry-adder-delay
+;;
+;; Dr = Df * n
+;; Df = 2Dh + Do
+;; Dh = (Da + Do)||(2Da + Di)
+;; Do = Di + Da + Di = 2Di + Da とすると(3.29)
+;; Da + Do = 2Di + 2Da > 2Da + Di より
+;; Dh = 2Da + 2Diとなる
+;; Df = 2 * (2Da + 2Di) + 2Di + Da
+;;    = 5Da + 6Di
+;; Dr = (5Da + 6Di) * n

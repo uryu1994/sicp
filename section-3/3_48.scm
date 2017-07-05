da5f381de6305d0fa6f8b26e3ac0420c41557250
uryu1994 <prices_over@uryu1994.work>
2017-07-05 20:48:24 +0900

    Fix account order of 3.48

--- section-3/3_48.scm
+++ section-3/3_48.scm
@@ -28,5 +28,5 @@
   (let ((serializer1 (account1 'serializer))
         (serializer2 (account2 'serializer)))
     (if (< (account1 'id) (account2 'id))
-        ((serializer2 (serializer1 exchange)) account2 account1)
+        ((serializer2 (serializer1 exchange)) account1 account2)
         ((serializer1 (serializer2 exchange)) account1 account2))))

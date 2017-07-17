b792b15ac8cac77e8825ea14b093d66c44b3c8d6
uryu1994 <prices_over@uryu1994.work>
2017-07-17 19:27:41 +0900

    Add 3.50

---
+++ section-3/3_50.scm
@@ -0,0 +1,7 @@
+(define (stream-map proc . argstreams)
+  (if (null? (car argstreams))
+      the-empty-stream
+      (cons
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))

fa82ac9670f4f6ba651f1dc1f8298d0df33f0ba9
uryu1994 <prices_over@uryu1994.work>
2017-07-18 00:43:39 +0900

    Add 3.54 and fix stream-map
    reference: http://wat-aro.hatenablog.com/entry/2015/12/09/025559

---
+++ section-3/3_54.scm
@@ -0,0 +1,15 @@
+(load "./stream")
+
+;; -- Include stream.scm
+;; (define (mul-streams s1 s2)
+;;   (stream-map * s1 s2))
+
+(define factorials (cons-stream 1
+                                (mul-streams integers factorials)))
+
+(stream-ref factorials 0)
+(stream-ref factorials 1)
+(stream-ref factorials 2)
+(stream-ref factorials 3)
+(stream-ref factorials 4)
+(stream-ref factorials 5)
--- section-3/stream.scm
+++ section-3/stream.scm
@@ -11,17 +11,18 @@
   (syntax-rules ()
     ((_ a b) (cons a (delay b)))))
 
-
 (define (stream-ref s n)
   (if (= n 0)
       (stream-car s)
       (stream-ref (stream-cdr s) (- n 1))))
 
-(define (stream-map proc s)
-  (if (stream-null? s)
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
       the-empty-stream
-      (cons-stream (proc (stream-car s))
-                   (stream-map proc (stream-cdr s)))))
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
 
 (define (stream-for-each proc s)
   (if (stream-null? s)
@@ -66,3 +67,15 @@
                  (set! already-run? true)
                  result)
           result))))
+
+;; 3.5.2 Infinite Stream
+(define ones (cons-stream 1 ones))
+
+(define integers (cons-stream 1 (add-streams ones integers)))
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+;; 3.54's Answer
+(define (mul-streams s1 s2)
+  (stream-map * s1 s2))

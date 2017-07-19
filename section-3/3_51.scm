bc9cd5e54f5b2ec32bdd2f6eb9b134fbc7dc1a2d
uryu1994 <prices_over@uryu1994.work>
2017-07-19 20:40:12 +0900

    Refactor Output of 3.51

--- section-3/3_51.scm
+++ section-3/3_51.scm
@@ -5,20 +5,15 @@
   x)
 
 (define x (stream-map show (stream-enumerate-interval 0 10)))
+;; 0x
 
-(define main
-  (begin
-    (display "(stream-ref x 5)")
-    (stream-ref x 5)
-    (display "(stream-ref x 7)")
-    (stream-ref x 7)))
-
-
-;; 0(stream-ref x 5)
+(stream-ref x 5)
 ;; 1
 ;; 2
 ;; 3
 ;; 4
-;; 5(stream-ref x 7)
+;; 55
+
+(stream-ref x 7)
 ;; 6
-;; 7
+;; 77

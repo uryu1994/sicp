4d89b185c50e11cb5d5e8f3deef63c234bd4452d
uryu1994 <prices_over@uryu1994.work>
2017-08-17 23:40:13 +0900

    Add 3.60

---
+++ section-3/3_60.scm
@@ -0,0 +1,22 @@
+(load "./stream")
+(load "./3_59")
+
+(define (mul-series s1 s2)
+  (cons-stream (* (stream-car s1) (stream-car s2))
+	       (add-streams (scale-stream (stream-cdr s2) (stream-car s1))
+			    (mul-series (stream-cdr s1) s2))))
+
+(stream-ref (add-streams (mul-series sine-series sine-series)
+			 (mul-series cosine-series cosine-series)) 0)
+
+(stream-ref (add-streams (mul-series sine-series sine-series)
+			 (mul-series cosine-series cosine-series)) 1)
+
+(stream-ref (add-streams (mul-series sine-series sine-series)
+			 (mul-series cosine-series cosine-series)) 2)
+
+(stream-ref (add-streams (mul-series sine-series sine-series)
+			 (mul-series cosine-series cosine-series)) 3)
+
+(stream-ref (add-streams (mul-series sine-series sine-series)
+			 (mul-series cosine-series cosine-series)) 4)

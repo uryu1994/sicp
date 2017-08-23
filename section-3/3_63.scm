c070f0c5fa9eabca4e54238e9181a06f599b7bd4
uryu1994 <prices_over@uryu1994.work>
2017-08-23 20:38:45 +0900

    Add 3.63

--- section-3/3_63.scm
+++ section-3/3_63.scm
@@ -2,9 +2,10 @@
   (cons-stream 1.0
                (stream-map (lambda (guess)
                              (sqrt-improve guess x))
-                           (sqrt-stream x))))
+                           (sqrt-stream x)))) ;; ここで環境が作られる
 
 ;; Louis版だと毎回ストリームを最初から生成することになる
 ;; そのため，再度計算が走ることになるためmemo化されてないことと同じになる
 
-;; delay実装がmemo-procの最適化を使わない場合も同様に，再計算されまくるため効率が悪い
+;; delay実装がmemo-procの最適化を使わなかったとしたらそもそもstream自体がmemo化されていないので，
+;; 効率は変わらない

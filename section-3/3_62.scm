220549efcecc5efb1b83803c0b906009a233fe33
uryu1994 <prices_over@uryu1994.work>
2017-08-23 20:16:41 +0900

    Fix 3.62

--- section-3/3_62.scm
+++ section-3/3_62.scm
@@ -5,7 +5,9 @@
 (define (div-series s1 s2)
   (if (= (stream-car s2) 0)
       (error "-- DIV-SERIES divide zero")
-      (mul-series s1 (invert-unit-series s2))))
+      (scale-stream (mul-series s1
+				(invert-unit-series (scale-stream s2 (/ 1 (stream-car s2)))))
+		    (/ 1 (stream-car s2)))))
 
 (define tan-series (div-series sine-series cosine-series))
 

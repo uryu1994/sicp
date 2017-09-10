05b7da3176ee2c5d0eb027c3b90293e65f077837
uryu1994 <prices_over@uryu1994.work>
2017-09-10 23:59:01 +0900

    Add 3.77

---
+++ section-3/3_77.scm
@@ -0,0 +1,18 @@
+(load "./stream")
+
+(define (solve f y0 dt)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (stream-map f y))
+  y)
+
+(define (integral delay-integrand initial-value dt)
+  (cons-stream initial-value
+	       (let ((integrand (force delay-integrand)))
+		 (if (stream-null? integrand)
+		     the-empty-stream
+		     (integral (delay (stream-cdr integrand))
+			       (+ (* dt (stream-car integrand))
+				  initial-value)
+			       dt)))))
+
+(stream-ref (solve (lambda (y) y) 1 0.001) 1000)

ab2c20c7ce0e3c853756a85a3d2eec34faf72998
uryu1994 <prices_over@uryu1994.work>
2017-11-22 20:31:53 +0900

    Fix 4.18

--- section-4/4_18.scm
+++ section-4/4_18.scm
@@ -5,14 +5,13 @@
 
 ;; 問題
 (define (solve f y0 dt)
-  (lambda (f y0 dt)
-    (let ((y '*unassigned*)
-	  (dy '*unassigned*))
-      (let ((a (integral (delay dy) y0 dt))
-	    (b (stream-map f y)))
-	(set! y a)
-	(set! dy b)
-	y))))
+  (let ((y '*unassigned*)
+	(dy '*unassigned*))
+    (let ((a (integral (delay dy) y0 dt))
+	  (b (stream-map f y)))
+      (set! y a)
+      (set! dy b)
+      y)))
 
 ;; aを束縛する際にdyが必要となる
 ;; この時点でdyは'*unssignedだが(delay dy)ではこの時点で評価されないと考える
@@ -21,12 +20,11 @@
 
 ;; 本文
 (define (solve f y0 dt)
-  (lambda (f y0 dt)
-    (let ((y '*unassigned*)
-	  (dy '*unassigned*))
-      (set! y (integral (delay dy) y0 dt))
-      (set! dy (stream-map f y))
-      y)))
+  (let ((y '*unassigned*)
+	(dy '*unassigned*))
+    (set! y (integral (delay dy) y0 dt))
+    (set! dy (stream-map f y))
+    y))
 
 ;; yにset!する際にdyが必要 しかし(delay dy)のためこの時点で評価はされないと考える
 ;; dyをset!する際にyが必要 delayがうまく動作すればエラーにならないはず

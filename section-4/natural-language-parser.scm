1f45d97a775d4e8a05ffa1f5ce2197200dba0094
uryu1994 <prices_over@uryu1994.work>
2018-01-22 02:23:09 +0900

    Fix natural-language-parser

--- section-4/natural-language-parser.scm
+++ section-4/natural-language-parser.scm
@@ -4,32 +4,6 @@
 	  '((define nouns '(noun student professor cat class))
 	    (define verbs '(verb studies lectures eats sleeps))
 	    (define articles '(article the a))
-	    
-	    (define (parse-sentence)
-	      (list 'sentence
-		    (parse-noun-phrase)
-		    (parse-word verbs)))
-
-	    (define (parse-noun-phrase)
-	      (list 'noun-phrase
-		    (parse-word articles)
-		    (parse-word nouns)))
-
-	    (define (parse-word word-list)
-	      (require (not (null? *unparsed*)))
-	      (require (memq (car *unparsed*) (cdr word-list)))
-	      (let ((found-word (car *unparsed*)))
-		(set! *unparsed* (cdr *unparsed*))
-		(list (car word-list) found-word)))
-
-	    (define *unparsed* '())
-
-	    (define (parse input)
-	      (set! *unparsed* input)
-	      (let ((sent (parse-sentence)))
-		(require (null? *unparsed*))
-		sent))
-	    
 	    (define prepositions '(prep for to in by with))
 
 	    (define (parse-prepositional-phrase)
@@ -62,4 +36,19 @@
 					 noun-phrase
 					 (parse-prepositional-phrase)))))
 	      (maybe-extend (parse-simple-noun-phrase)))
+	    
+	    (define (parse-word word-list)
+	      (require (not (null? *unparsed*)))
+	      (require (memq (car *unparsed*) (cdr word-list)))
+	      (let ((found-word (car *unparsed*)))
+		(set! *unparsed* (cdr *unparsed*))
+		(list (car word-list) found-word)))
+
+	    (define *unparsed* '())
+
+	    (define (parse input)
+	      (set! *unparsed* input)
+	      (let ((sent (parse-sentence)))
+		(require (null? *unparsed*))
+		sent))	    
 	    ))

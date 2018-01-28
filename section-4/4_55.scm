3e7e9a81e464e8b7139eb8a1466a148bd997444e
uryu1994 <prices_over@uryu1994.work>
2018-01-29 01:46:44 +0900

    Add 4.55 and microshaft database

---
+++ section-4/4_55.scm
@@ -0,0 +1,27 @@
+(load "./microshaft")
+
+(query-driver-loop)
+
+;; a
+(supervisor ?x (Bitdiddle Ben))
+
+;;; Query results:
+(supervisor (Tweakit Lem E) (Bitdiddle Ben))
+(supervisor (Fect Cy D) (Bitdiddle Ben))
+(supervisor (Hacker Alyssa P) (Bitdiddle Ben))
+
+
+;; b
+(job ?x (accounting . ?type))
+
+;;; Query results:
+(job (Cratchet Robert) (accounting scrivener))
+(job (Scrooge Eben) (accounting chief accountant))
+
+;; c
+(address ?x (Slumerville . ?where))
+
+;;; Query results:
+(address (Aull DeWitt) (Slumerville (Onion Square) 5))
+(address (Reasoner Louis) (Slumerville (Pine Tree Road) 80))
+

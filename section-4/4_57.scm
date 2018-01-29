cbb890547fd18bb4dbb794d8486ac33f229405cc
uryu1994 <prices_over@uryu1994.work>
2018-01-30 02:02:03 +0900

    rule is delay eval

--- section-4/4_57.scm
+++ section-4/4_57.scm
@@ -3,14 +3,14 @@
 (query-driver-loop)
 
 (assert! (rule (same ?x ?x)))
-(assert! (rule (replace ?person-1 ?person-2)
+(assert! (rule (replace ?person1 ?person2)
                (and
-                (or (and (job ?person-1 ?job)
-                         (job ?person-2 ?job))
-                    (and (job ?person-1 ?job-1)
-                         (job ?person-2 ?job-2)
-                         (can-do-job ?job-1 ?job-2)))
-                (not (same ?person-1 ?person-2)))))
+                (or (and (job ?person1 ?job)
+                         (job ?person2 ?job))
+                    (and (job ?person1 ?job1)
+                         (job ?person2 ?job2)
+                         (can-do-job ?job2 ?job1)))
+                (not (same ?person1 ?person2)))))
 
 ;; a
 (replace (Fect Cy D) ?who)
--- section-4/microshaft.scm
+++ section-4/microshaft.scm
@@ -54,20 +54,4 @@
 
             (can-do-job (administration secretary)
                         (administration big wheel))
-
-            ;; (rule (lives-near ?person-1 ?person-2)
-            ;;       (and (address ?person-1 (?town . ?rest-1))
-            ;;            (address ?person-2 (?town . ?rest-2))
-            ;;            (not (same ?person-1 ?person-2))))
-            
-            ;; (rule (same ?x ?x))
-
-            ;; (rule (wheel ?person)
-            ;;       (and (supervisor ?middle-manager ?person)
-            ;;            (supervisor ?x ?middle-manager)))
-
-            ;; (rule (outranked-by ?staff-person ?boss)
-            ;;       (or (supervisor ?staff-person ?boss)
-            ;;           (and (supervisor ?staff-person ?middle-manager)
-            ;;                (outranked-by ?middle-manager ?boss))))
             ))

0b8892dfe7538de18b64aba1db7b3aff97371d96
uryu1994 <prices_over@uryu1994.work>
2018-04-04 01:06:29 +0900

    Solve 5_03

--- section-5/5_03.scm
+++ section-5/5_03.scm
@@ -24,3 +24,19 @@
  (goto (label sqrt-loop)))
 
 ;; これらを算術演算を使って展開
+(controller
+ sqrt-loop
+ (assign x (op read))
+ (assign guess (const 1.0))
+ test-good-enough
+ (assign t1 (op *) (reg guess) (reg guess))
+ (assign t2 (op -) (reg t1) (reg x))
+ (assign t3 (op abs) (reg t2))
+ (test (op <) (reg t3) (const 0.001))
+ (branch (label sqrt-loop-done))
+ (assign t4 (op /) (reg x) (reg guess))
+ (assign guess (op average) (reg guess) (reg t4))
+ (goto (label test-good-enough))
+ sqrt-loop-done
+ (perform (op print) (reg guess))
+ (goto (label sqrt-loop)))

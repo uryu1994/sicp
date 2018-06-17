e68fbd19d32e7acb957a64084c797eec972e7fc3
uryu1994 <prices_over@uryu1994.work>
2018-06-18 00:27:33 +0900

    Add 5.41

---
+++ section-5/5_41.scm
@@ -0,0 +1,27 @@
+(load "./no-apply-eval")
+
+(define (make-address fnum dnum) (list fnum dnum))
+(define (frame-number address) (car address))
+(define (displacement-number address) (cadr address))
+
+(define (find-variable var ct-env)
+  (define (frame-iter frames frame-num)
+    (if (null? frames)
+        'not-found
+        (let ((addr (scan-iter (car frames) frame-num 0)))
+          (if (null? addr)
+              (frame-iter (cdr frames) (+ frame-num 1))
+              addr))))
+  (define (scan-iter frame frame-num disp-num)
+    (if (null? frame)
+        '()
+        (if (eq? var (car frame))
+            (make-address frame-num disp-num)
+            (scan-iter (cdr frame) frame-num (+ disp-num 1)))))
+  (frame-iter ct-env 0))
+
+(find-variable 'c '((y z) (a b c d e) (x y)))
+
+(find-variable 'x '((y z) (a b c d e) (x y)))
+
+(find-variable 'w '((y z) (a b c d e) (x y)))
